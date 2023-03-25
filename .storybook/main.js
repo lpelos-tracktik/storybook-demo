a493a7954574d1ca862b8c628374c676ba8a9570
Lucas Pélos <lucas.pelos@tracktik.com>
2023-03-25 16:24:42 -0400

    test coverage

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,19 +1,20 @@
 module.exports = {
-  "stories": [
+  stories: [
     "../src/**/*.stories.mdx",
     "../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
-  "addons": [
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
+    "@storybook/addon-coverage"
   ],
-  "framework": "@storybook/vue",
-  "core": {
-    "builder": "@storybook/builder-vite"
+  framework: "@storybook/vue",
+  core: {
+    builder: "@storybook/builder-vite"
   },
-  "features": {
-    "interactionsDebugger": true, // 👈 Enable playback controls
-    "storyStoreV7": true,
+  features: {
+    interactionsDebugger: true, // 👈 Enable playback controls
+    storyStoreV7: true,
   }
 }
